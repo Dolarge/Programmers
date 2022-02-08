0f45c141dbef7eec3b1f55d719257c9adb1125bf
dh.cheon <dohyun722@daum.net>
2022-02-08 17:47:13 +0900

    초기 커밋.

---
+++ BiggestNum/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace BiggestNum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = {10,12,231};
+            Solution solution = new Solution();
+            solution.Function(test);
+
+
+
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(int[] numbers)
+        {
+            string answer = "";
+
+            StringBuilder sb = new StringBuilder();
+            Array.Sort(numbers, (x, y) =>
+                    string.Compare(y.ToString() + x.ToString(), x.ToString() + y.ToString()));
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sb.Append(numbers[i].ToString());
+            }
+
+            answer = sb.ToString();
+
+            int count = 0;
+
+            foreach (var item in answer)
+            {
+                if (item == '0')
+                {
+                    count++;
+                }
+            }
+
+            if (count == answer.Length)
+            {
+                return "0";
+            }
+            else
+            {
+                return answer;
+            }
+
+            return answer;
+        }
+    }
+
+}
+
